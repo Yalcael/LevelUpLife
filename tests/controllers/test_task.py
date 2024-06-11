d123a8d67671408adda59093ff2e0b93f2ddbed2
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-06-11 21:28:41 +0200

    LevelUpLife in Progress

--- tests/controllers/test_task.py
+++ tests/controllers/test_task.py
@@ -3,6 +3,7 @@ from faker import Faker
 from sqlmodel import Session, select
 
 from leveluplife.controllers.task import TaskController
+from leveluplife.models.error import TaskAlreadyExistsError
 from leveluplife.models.task import TaskCreate, Task
 
 
@@ -27,3 +28,47 @@ async def test_create_task(
     assert result.description == task_create.description == task.description
     assert result.completed == task_create.completed == task.completed
     assert result.category == task_create.category == task.category
+
+
+@pytest.mark.asyncio
+async def test_create_task_already_exists_error(
+    task_controller: TaskController, faker: Faker
+) -> None:
+    task_create = TaskCreate(
+        title=faker.word(),
+        description=faker.text(max_nb_chars=400),
+        completed=faker.boolean(),
+        category=faker.word(),
+    )
+
+    # Act
+    await task_controller.create_task(task_create)
+
+    # Assert
+    with pytest.raises(TaskAlreadyExistsError):
+        await task_controller.create_task(task_create)
+
+
+@pytest.mark.asyncio
+async def test_get_tasks(task_controller: TaskController, faker: Faker) -> None:
+    number_tasks = 5
+    created_tasks = []
+    for _ in range(number_tasks):
+        task_create = TaskCreate(
+            title=faker.word(),
+            description=faker.text(max_nb_chars=400),
+            completed=faker.boolean(),
+            category=faker.word(),
+        )
+        created_task = await task_controller.create_task(task_create)
+        created_tasks.append(created_task)
+
+    all_tasks = await task_controller.get_tasks()
+
+    assert len(all_tasks) == number_tasks
+
+    for i, created_task in enumerate(created_tasks):
+        assert all_tasks[i].title == created_task.title
+        assert all_tasks[i].description == created_task.description
+        assert all_tasks[i].completed == created_task.completed
+        assert all_tasks[i].category == created_task.category
--- tests/routes/test_task.py
+++ tests/routes/test_task.py
@@ -8,6 +8,7 @@ from starlette.testclient import TestClient
 
 from leveluplife.controllers.task import TaskController
 from leveluplife.dependencies import get_task_controller
+from leveluplife.models.error import TaskAlreadyExistsError
 from leveluplife.models.task import Task
 
 
@@ -44,3 +45,75 @@ async def test_create_test(
         "completed": mock_task.completed,
         "category": mock_task.category,
     }
+
+
+@pytest.mark.asyncio
+async def test_create_task_raise_task_already_exists_error(
+    task_controller: TaskController, client: TestClient, app: FastAPI
+) -> None:
+    task_data = {
+        "title": "Supermarket",
+        "description": "John Doe is going to the supermarket",
+        "completed": "False",
+        "category": "Groceries",
+    }
+
+    def _mock_create_task():
+        task_controller.create_task = AsyncMock(
+            side_effect=TaskAlreadyExistsError(title=task_data["title"])
+        )
+        return task_controller
+
+    app.dependency_overrides[get_task_controller] = _mock_create_task
+
+    create_task_response = client.post("/tasks", json=task_data)
+    assert create_task_response.status_code == 409
+    assert create_task_response.json() == {
+        "name": "TaskAlreadyExistsError",
+        "message": f"Task with the title {task_data['title']} already exists.",
+        "status_code": 409,
+    }
+
+
+@pytest.mark.asyncio
+async def test_get_tasks(
+    task_controller: TaskController, client: TestClient, app: FastAPI
+) -> None:
+    mock_tasks = [
+        Task(
+            id=uuid.uuid4(),
+            created_at=datetime(2020, 1, 1),
+            title="Supermarket",
+            description="John Doe is going to the supermarket",
+            completed=False,
+            category="Groceries",
+        ),
+        Task(
+            id=uuid.uuid4(),
+            created_at=datetime(2022, 2, 2),
+            title="Video games",
+            description="Playing video games",
+            completed=True,
+            category="Fun",
+        ),
+    ]
+
+    def _mock_get_tasks():
+        task_controller.get_tasks = AsyncMock(return_value=mock_tasks)
+        return task_controller
+
+    app.dependency_overrides[get_task_controller] = _mock_get_tasks
+
+    get_task_response = client.get("/tasks")
+    assert get_task_response.status_code == 200
+    assert get_task_response.json() == [
+        {
+            "id": str(task.id),
+            "created_at": task.created_at.isoformat(),
+            "title": task.title,
+            "description": task.description,
+            "completed": task.completed,
+            "category": task.category,
+        }
+        for task in mock_tasks
+    ]
