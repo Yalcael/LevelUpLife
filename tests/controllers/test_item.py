9f7e6c5e47643cda1526c48f57b6b31a5d742f5a
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-07-13 23:41:26 +0200

    LevelUpLife in Progress

--- tests/controllers/test_item.py
+++ tests/controllers/test_item.py
@@ -1,15 +1,20 @@
 import pytest
 from faker import Faker
 from sqlmodel import select, Session
-
+import random
 from leveluplife.controllers.item import ItemController
+from leveluplife.controllers.user import UserController
 from leveluplife.models.error import (
     ItemAlreadyExistsError,
     ItemNotFoundError,
     ItemNameNotFoundError,
+    ItemAlreadyInUserError,
+    ItemInUserNotFoundError,
 )
 from leveluplife.models.item import ItemCreate, ItemUpdate
+from leveluplife.models.relationship import UserItemLinkCreate
 from leveluplife.models.table import Item
+from leveluplife.models.user import UserCreate, Tribe
 
 
 @pytest.mark.asyncio
@@ -256,3 +261,171 @@ async def test_delete_item_raise_item_not_found_error(
 
     with pytest.raises(ItemNotFoundError):
         await item_controller.delete_item(nonexistent_item_id)
+
+
+@pytest.mark.asyncio
+async def test_give_item_to_user(
+    item_controller: ItemController, faker: Faker, user_controller: UserController
+) -> None:
+    # Prepare
+    item_create = ItemCreate(
+        name=faker.unique.word(),
+        description=faker.text(max_nb_chars=300),
+        price_sell=faker.random_int(min=0, max=100),
+        strength=faker.random_int(min=0, max=100),
+        intelligence=faker.random_int(min=0, max=100),
+        agility=faker.random_int(min=0, max=100),
+        wise=faker.random_int(min=0, max=100),
+        psycho=faker.random_int(min=0, max=100),
+    )
+    created_item = await item_controller.create_item(item_create)
+
+    user_create = UserCreate(
+        username=faker.unique.user_name()[:18],
+        email=faker.unique.email(),
+        password=faker.password(),
+        tribe=random.choice(list(Tribe)),
+    )
+    created_user = await user_controller.create_user(user_create)
+
+    # Act
+    await item_controller.give_item_to_user(
+        created_item.id, UserItemLinkCreate(user_ids=[created_user.id])
+    )
+
+    # Assert
+    retrieved_item = await item_controller.get_item_by_id(created_item.id)
+    assert created_user.id in [user.id for user in retrieved_item.users]
+
+
+@pytest.mark.asyncio
+async def test_give_item_to_user_raise_item_already__in_user_error(
+    item_controller: ItemController, faker: Faker, user_controller: UserController
+) -> None:
+    # Prepare
+    item_create = ItemCreate(
+        name=faker.unique.word(),
+        description=faker.text(max_nb_chars=300),
+        price_sell=faker.random_int(min=0, max=100),
+        strength=faker.random_int(min=0, max=100),
+        intelligence=faker.random_int(min=0, max=100),
+        agility=faker.random_int(min=0, max=100),
+        wise=faker.random_int(min=0, max=100),
+        psycho=faker.random_int(min=0, max=100),
+    )
+    created_item = await item_controller.create_item(item_create)
+
+    user_create = UserCreate(
+        username=faker.unique.user_name()[:18],
+        email=faker.unique.email(),
+        password=faker.password(),
+        tribe=random.choice(list(Tribe)),
+    )
+    created_user = await user_controller.create_user(user_create)
+
+    # Act
+    await item_controller.give_item_to_user(
+        created_item.id, UserItemLinkCreate(user_ids=[created_user.id])
+    )
+
+    # Assert
+    with pytest.raises(ItemAlreadyInUserError):
+        await item_controller.give_item_to_user(
+            created_item.id, UserItemLinkCreate(user_ids=[created_user.id])
+        )
+
+
+@pytest.mark.asyncio
+async def test_give_item_to_user_raise_item_not_found_error(
+    item_controller: ItemController, faker: Faker, user_controller: UserController
+) -> None:
+    # Prepare
+    non_existent_item_id = faker.uuid4()
+
+    user_create = UserCreate(
+        username=faker.unique.user_name()[:18],
+        email=faker.unique.email(),
+        password=faker.password(),
+        tribe=random.choice(list(Tribe)),
+    )
+    created_user = await user_controller.create_user(user_create)
+
+    user_item_link_create = UserItemLinkCreate(user_ids=[created_user.id])
+
+    # Assert
+    with pytest.raises(ItemNotFoundError):
+        await item_controller.give_item_to_user(
+            non_existent_item_id, user_item_link_create=user_item_link_create
+        )
+
+
+@pytest.mark.asyncio
+async def test_remove_item_from_user(
+    item_controller: ItemController, user_controller: UserController, faker: Faker
+) -> None:
+    # Prepare
+    item_create = ItemCreate(
+        name=faker.unique.word(),
+        description=faker.text(max_nb_chars=300),
+        price_sell=faker.random_int(min=0, max=100),
+        strength=faker.random_int(min=0, max=100),
+        intelligence=faker.random_int(min=0, max=100),
+        agility=faker.random_int(min=0, max=100),
+        wise=faker.random_int(min=0, max=100),
+        psycho=faker.random_int(min=0, max=100),
+    )
+    created_item = await item_controller.create_item(item_create)
+
+    user_create = UserCreate(
+        username=faker.unique.user_name()[:18],
+        email=faker.unique.email(),
+        password=faker.password(),
+        tribe=random.choice(list(Tribe)),
+    )
+    created_user = await user_controller.create_user(user_create)
+
+    user_item_link_create = UserItemLinkCreate(user_ids=[created_user.id])
+    await item_controller.give_item_to_user(created_item.id, user_item_link_create)
+
+    # Act
+    await item_controller.remove_item_from_user(created_item.id, created_user.id)
+
+    # Assert
+    retrieved_item = await item_controller.get_item_by_id(created_item.id)
+    assert created_user.id not in [user.id for user in retrieved_item.users]
+
+
+@pytest.mark.asyncio
+async def test_remove_item_from_user_raise_item_in_user_not_found_error(
+    item_controller: ItemController, user_controller: UserController, faker: Faker
+) -> None:
+    # Prepare
+    item_create = ItemCreate(
+        name=faker.unique.word(),
+        description=faker.text(max_nb_chars=300),
+        price_sell=faker.random_int(min=0, max=100),
+        strength=faker.random_int(min=0, max=100),
+        intelligence=faker.random_int(min=0, max=100),
+        agility=faker.random_int(min=0, max=100),
+        wise=faker.random_int(min=0, max=100),
+        psycho=faker.random_int(min=0, max=100),
+    )
+    created_item = await item_controller.create_item(item_create)
+
+    user_create = UserCreate(
+        username=faker.unique.user_name()[:18],
+        email=faker.unique.email(),
+        password=faker.password(),
+        tribe=random.choice(list(Tribe)),
+    )
+    created_user = await user_controller.create_user(user_create)
+
+    user_item_link_create = UserItemLinkCreate(user_ids=[created_user.id])
+    await item_controller.give_item_to_user(created_item.id, user_item_link_create)
+
+    # Act
+    await item_controller.remove_item_from_user(created_item.id, created_user.id)
+
+    # Assert
+    with pytest.raises(ItemInUserNotFoundError):
+        await item_controller.remove_item_from_user(created_item.id, created_user.id)
