2649233b27181759b8ba6661eacb1c58d532dc13
Yalcael <vincent.lemaire1999@gmail.com>
2024-08-11 15:56:37 +0200

    LevelUpLife in Progress

--- tests/controllers/test_rating.py
+++ tests/controllers/test_rating.py
@@ -6,7 +6,7 @@ from leveluplife.controllers.rating import RatingController
 from leveluplife.controllers.task import TaskController
 from leveluplife.controllers.user import UserController
 from leveluplife.models.error import RatingAlreadyExistsError, RatingNotFoundError
-from leveluplife.models.rating import RatingCreate
+from leveluplife.models.rating import RatingCreate, RatingUpdate
 from leveluplife.models.table import Rating
 from leveluplife.models.task import TaskCreate
 from leveluplife.models.user import Tribe, UserCreate
@@ -210,3 +210,62 @@ async def test_get_rating_by_id_raise_rating_not_found_error(
     non_existent_rating_id = faker.uuid4()
     with pytest.raises(RatingNotFoundError):
         await rating_controller.get_rating_by_id(non_existent_rating_id)
+
+
+@pytest.mark.asyncio
+async def test_update_rating(
+    rating_controller: RatingController,
+    user_controller: UserController,
+    task_controller: TaskController,
+    faker: Faker,
+) -> None:
+    user_create = UserCreate(
+        username=faker.unique.user_name(),
+        email=faker.unique.email(),
+        password=faker.password(),
+        tribe=Tribe.NOSFERATI,
+    )
+    user = await user_controller.create_user(user_create)
+
+    task_create = TaskCreate(
+        title=faker.unique.word(),
+        description=faker.text(max_nb_chars=400),
+        completed=faker.boolean(),
+        category=faker.word(),
+        user_id=user.id,
+    )
+    task = await task_controller.create_task(task_create)
+
+    rating_create = RatingCreate(
+        task_id=task.id,
+        user_id=user.id,
+        rating=faker.random_int(min=0, max=10),
+    )
+    created_rating = await rating_controller.create_rating(rating_create)
+
+    rating_update = RatingUpdate(
+        rating=faker.random_int(min=0, max=10),
+    )
+
+    updated_rating = await rating_controller.update_rating(
+        created_rating.id, rating_update
+    )
+
+    assert updated_rating.id == created_rating.id
+    assert updated_rating.task_id == created_rating.task_id
+    assert updated_rating.user_id == created_rating.user_id
+    assert updated_rating.rating == rating_update.rating
+
+
+@pytest.mark.asyncio
+async def test_update_rating_raise_rating_not_found_error(
+    rating_controller: RatingController, faker: Faker
+) -> None:
+    rating_update = RatingUpdate(
+        rating=faker.random_int(min=0, max=10),
+    )
+
+    nonexistent_rating_id = faker.uuid4()
+
+    with pytest.raises(RatingNotFoundError):
+        await rating_controller.update_rating(nonexistent_rating_id, rating_update)
