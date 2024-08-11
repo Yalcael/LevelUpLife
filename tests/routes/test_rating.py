523ca2875627f75fb13abbe60cc7a5e3c0bf8158
Yalcael <vincent.lemaire1999@gmail.com>
2024-08-11 16:06:22 +0200

    LevelUpLife in Progress

--- tests/routes/test_rating.py
+++ tests/routes/test_rating.py
@@ -9,6 +9,7 @@ from starlette.testclient import TestClient
 
 from leveluplife.controllers.rating import RatingController
 from leveluplife.models.error import RatingAlreadyExistsError, RatingNotFoundError
+from leveluplife.models.rating import RatingUpdate
 from leveluplife.models.table import Rating, Task, User
 from leveluplife.models.user import Tribe
 
@@ -220,6 +221,92 @@ async def test_get_rating_by_id_raise_rating_not_found_error(
     get_rating_by_id_response = client.get(f"/ratings/{mock_rating_id}")
     assert get_rating_by_id_response.status_code == 404
     assert get_rating_by_id_response.json() == {
+        "message": f"Rating with ID {mock_rating_id} not found",
+        "name": "RatingNotFoundError",
+        "status_code": 404,
+    }
+
+
+@pytest.mark.asyncio
+async def test_update_rating(
+    rating_controller: RatingController, client: TestClient, app: FastAPI
+) -> None:
+    mock_user = User(
+        id=uuid.uuid4(),
+        username="test_user",
+        email="test@gmail.com",
+        tribe=Tribe.NEUTRALS,
+        created_at=datetime(2020, 1, 1),
+        strength=5,
+        intelligence=5,
+        agility=5,
+        wise=5,
+        psycho=5,
+        experience=0,
+    )
+
+    mock_task = Task(
+        id=uuid.uuid4(),
+        created_at=datetime(2020, 1, 1),
+        title="Supermarket",
+        description="John Doe is going to the supermarket",
+        completed=False,
+        category="Groceries",
+        user_id=mock_user.id,
+    )
+
+    mock_rating_id = uuid.uuid4()
+
+    rating_update_data = {
+        "rating": 3,
+    }
+
+    updated_rating = Rating(
+        id=mock_rating_id,
+        **rating_update_data,
+        created_at=datetime(2020, 1, 1),
+        task_id=mock_task.id,
+        user_id=mock_user.id,
+    )
+
+    def _mock_update_rating():
+        rating_controller.update_rating = AsyncMock(return_value=updated_rating)
+        return rating_controller
+
+    app.dependency_overrides[get_rating_controller] = _mock_update_rating
+
+    update_rating_response = client.patch(f"/ratings/{mock_rating_id}", json=rating_update_data)
+    assert update_rating_response.status_code == 200
+    assert update_rating_response.json() == {
+        "id": str(updated_rating.id),
+        "created_at": updated_rating.created_at.isoformat(),
+        "task_id": str(updated_rating.task_id),
+        "user_id": str(updated_rating.user_id),
+        "rating": updated_rating.rating,
+    }
+
+
+@pytest.mark.asyncio
+async def test_update_rating_raise_rating_not_found_error(
+    rating_controller: RatingController, client: TestClient, app: FastAPI
+) -> None:
+    mock_rating_id = uuid.uuid4()
+
+    rating_update_data = {
+        "rating": 3,
+    }
+
+    def _mock_update_rating():
+        rating_controller.update_rating = AsyncMock(
+            side_effect=RatingNotFoundError(rating_id=mock_rating_id)
+        )
+        return rating_controller
+
+    app.dependency_overrides[get_rating_controller] = _mock_update_rating
+
+    update_rating_response = client.patch(f"/ratings/{mock_rating_id}", json=rating_update_data)
+    assert update_rating_response.status_code == 404
+    assert update_rating_response.json() == {
         "message": f"Rating with ID {mock_rating_id} not found",
         "name": "RatingNotFoundError",
         "status_code": 404,
